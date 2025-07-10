2d78054421b975004c880a7543b71f6b1dc8e6fc
Viki <vivekrawatyt01@gmail.com>
2025-07-10 12:56:01 +0530

    Added Dockerfile to fix OpenCV deployment issue

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from app import views
 
@@ -5,12 +6,11 @@ app = Flask(__name__)
 
 app.add_url_rule(rule='/', endpoint='home', view_func=views.index)
 app.add_url_rule(rule='/app/', endpoint='app', view_func=views.app)
-app.add_url_rule(
-    rule='/app/gender/',
-    endpoint='gender',
-    view_func=views.genderapp,
-    methods=['GET', 'POST']
-)
+app.add_url_rule(rule='/app/gender/', 
+                 endpoint='gender',
+                 view_func=views.genderapp,
+                 methods=['GET', 'POST'])
 
-# DO NOT include app.run() here if you're using gunicorn
-# gunicorn runs the app by importing `app` from this file
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # use PORT if set, else default to 8080
+    app.run(host="0.0.0.0", port=port)
